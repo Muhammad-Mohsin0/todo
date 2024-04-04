dc823a00e26b8bddc987bb5ec693cf6d7a62d986
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2024-04-04 05:09:17 +0500

    todo react

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+
+function App() {
+
+  const [newItem, setNewItem] = useState("")
+  
+  return (
+    <>
+     <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+      <label htmlFor="item"> New Item </label>
+      <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
+      </div>
+      <button className="btn"> Add</button>
+     </form>
+     <h1 className="header">Todo List</h1>
+     <ul className="list">
+     <li>
+       <label>
+        <input type="checkbox"/>
+        item 1
+       </label>
+       <button className="btn btn-danger">Delete</button>
+     </li>
+     <li>
+       <label>
+        <input type="checkbox"/>
+        item 2
+       </label>
+       <button className="btn btn-danger">Delete</button>
+     </li>
+      
+     </ul>
+    </>
+  );
+}
+
+export default App;
