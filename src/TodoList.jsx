56b1603085df33caafbeb732731d2d0f7b0844f2
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2024-04-04 11:27:10 +0500

    todo react

---
+++ src/TodoList.jsx
@@ -0,0 +1,21 @@
+import TodoItem from "./TodoItem";
+
+function TodoList({todos,toggleTodo,deleteTodo}){
+    return (
+        <ul className="list">
+     {todos.length === 0 && "No Todos"}
+     {todos.map(todo =>{ //for repeating the array list
+      return (
+     <TodoItem {...todo}
+     key={todo.id} 
+     toggleTodo={toggleTodo} 
+     deleteTodo={deleteTodo}
+    //  id={todo.id} completed={todo.completed} title={todo.title} 
+      />
+      )
+     })}
+     </ul>
+    )
+}
+
+export default TodoList;
